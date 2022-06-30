d4053ae62266c9ba35faa77fd985079d22228083
dltndn <james98099@gmail.com>
2022-06-30 17:55:19 +0900

    feat: Home페이지 Navigation 디자인 적용

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,49 @@
+import styled from "styled-components";
+
 const Navigation = ()=> {
 return (<div>
-    <div>코멘토 쇼핑</div>
+    <NavigationStyle>
+        <NavigationTitle>
+          <div>코멘토 쇼핑</div>
+        </NavigationTitle>
+    </NavigationStyle>
+    <NavigationLine />
+    
 </div>)
 };
+
+const NavigationStyle = styled.div`
+  width: 390px;
+  height: 66px;
+  left: 0px;
+  top: 0px;
+
+  
+`;
+
+const NavigationTitle = styled.div`
+  position: absolute;
+  height: 26px;
+  left: 152px;
+  top: 20px;
+  font-family: 'Noto Sans KR';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 26px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #000000;
+`;
+
+const NavigationLine = styled.div`
+  position: absolute;
+  width: 400px;
+  height: 1px;
+  left: -5px;
+  top: 63px;
+  background: #EEEEEE;
+`;
 
 export default Navigation;
