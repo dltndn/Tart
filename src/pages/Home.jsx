af4cd15a0c6e14a3665349b59988ec6f7442112a
dltndn <james98099@gmail.com>
2022-07-08 13:44:19 +0900

    feat: 1초 후 상품 초기화되는 로직 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,10 @@ import styled from "styled-components";
 import Navigation from "../components/Navigation";
 import ThemeButton from "../components/ThemeButton";
 import ProductCard from "../components/ProductCard";
-import {mockTheme1Produdcts, mockTheme2Produdcts} from '../data/mockData';
-import { useState } from "react"
+import { mockTheme1Produdcts, mockTheme2Produdcts } from "../data/mockData";
+import { useEffect, useState } from "react";
 
 const Home = () => {
-
   const [products, setProducts] = useState();
 
   const onClickThemeButton = (themeId) => {
@@ -16,31 +15,45 @@ const Home = () => {
       setProducts(mockTheme2Produdcts);
     }
   };
-  
+
+  useEffect(() => {
+    setTimeout(() => {
+      setProducts(mockTheme1Produdcts);
+    }, 1000);
+  }, []);
+
   return (
     <div>
       <PageStyle>
         <div>
           <Navigation />
           <ThemeButtonFrame>
-            <ThemeButton name={"# 겨울방한템"} orederNum={0} onClick={() => onClickThemeButton(1)}/>
-            <ThemeButton name={"# 여름더워요"} orederNum={1} onClick={() => onClickThemeButton(2)}/>
+            <ThemeButton
+              name={"# 겨울방한템"}
+              orederNum={0}
+              onClick={() => onClickThemeButton(1)}
+            />
+            <ThemeButton
+              name={"# 여름더워요"}
+              orederNum={1}
+              onClick={() => onClickThemeButton(2)}
+            />
           </ThemeButtonFrame>
           <ThemeButtonLine />
         </div>
         <div style={{ position: "absolute" }}>
-            {products ? (
-              products.map((product) => (
-                <ProductCard
-                  key={product.id}
-                  name={product.name}
-                  description={product.description}
-                  thumbnail={product.thumbnail}
-                  />
-                  ))
-            ) : (
-              <div>loading ...</div>
-            )}
+          {products ? (
+            products.map((product) => (
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                description={product.description}
+                thumbnail={product.thumbnail}
+              />
+            ))
+          ) : (
+            <div>loading ...</div>
+          )}
         </div>
       </PageStyle>
     </div>
